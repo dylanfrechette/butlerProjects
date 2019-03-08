cdd5c242c641778573103588ca500cc4539299ff
Dylan Bakr <dylan_bakr@comcast.net>
2019-03-07 22:18:45 -0500

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,11 @@
+public class Customer {
+    int toa;
+    int partySize;
+    String partyName;
+
+    public Customer(int time, int size, String who){
+        toa=time;
+        partySize=size;
+        partyName=who;
+    }
+}
