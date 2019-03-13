79ebf8a9a3e9277c7031715db937ca5d54f88419
Dylan Frechette <47864289+dylanfrechette@users.noreply.github.com>
2019-03-13 13:24:11 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,11 +1,24 @@
-public class Customer {
-    int toa;
+//Current version full of issues bc I suck ass at coding
+public class Customer 
+{
+	int toa;
     int partySize;
     String partyName;
-
-    public Customer(int time, int size, String who){
+    int seatTime;
+    public Customer(int time, int size, String who)
+    {
         toa=time;
         partySize=size;
         partyName=who;
     }
+    public int Wait(int toa, int seatTime)
+    {
+    	//Need to implement loop, just setting groundwork
+    	int waitTime;
+    	waitTime=seatTime-toa;
+    	return waitTime;
+    }
+    //full of issues, just putting basic idea in place
+    public boolean seated=false;
+    if(int currentTime>=seatTime) {seated=true;}
 }
