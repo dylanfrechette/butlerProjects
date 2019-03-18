c760795b1c7e1187cb4f44142ba59e6c9759c4e9
Dylan Bakr <dylan_bakr@comcast.net>
2019-03-18 12:04:47 -0500

    Update LaFoodMain.java

--- LaFoodMain.java
+++ LaFoodMain.java
@@ -2,20 +2,29 @@ import java.io.*;
 import java.util.*;
 import java.math.*;
 
-public class LaFoodMain 
+public class LaRestaurant extends Customer
 {
+    public LaRestaurant(Scanner fin){
+        super(fin);
+    }
 
-	public static void main(String[] args) throws FileNotFoundException
-	{
-		Scanner cin= new Scanner(System.in);
-		String fileName;
-		System.out.println("Please enter your file name: ");
-		
-		fileName=cin.nextLine();
-		Scanner fin= new Scanner(new FileReader(fileName));
-		//int lengthoffile=Integer.parseInt(fin.nextLine());
-		
-
-	}
-
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        String nextCommand = " ";
+        Scanner cin= new Scanner(System.in);
+        String fileName;
+        System.out.println("Please enter your file name: ");
+        fileName=cin.nextLine();
+        Scanner fin= new Scanner(new FileReader(fileName));
+	PartyQueue Q = new PartyQueue();
+        //int lengthoffile=Integer.parseInt(fin.nextLine());
+	nextCommand=fin.next();
+        /* while (!(nextCommand.equalsIgnoreCase("q"))) {
+            if (nextCommand.equalsIgnoreCase("a")){
+	    Q.enqueue(new Customer(fin));
+	    System.out.println("Please wait to be seated " + Q.getFront());
+	    }
+	    if(nextCommand.equalsIgnoreCase("t")){Q.dequeue()}
+        } */
+    }
 }
