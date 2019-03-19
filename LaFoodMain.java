36f806e04410ea79eecd13db8e9f8b1ca4e4f95a
Dylan Frechette <47864289+dylanfrechette@users.noreply.github.com>
2019-03-18 20:35:59 -0400

    Update LaFoodMain.java

--- LaFoodMain.java
+++ LaFoodMain.java
@@ -5,19 +5,46 @@ import java.math.*;
 public class LaFoodMain extends Customer
 {
 
-	public LaFoodMain(int time, int size, String who)
+	public LaFoodMain(int time, int size, String who, int seat)
 	{
-		super(time, size, who);
+		super(time, size, who, seat);
 	}
 
 	public static void main(String[] args) throws FileNotFoundException
 	{
+		String nextCommand=" ";
 		Scanner cin= new Scanner(System.in);
 		String fileName;
 		System.out.println("Please enter your file name: ");
 		
 		fileName=cin.nextLine();
 		Scanner fin= new Scanner(new FileReader(fileName));
+//		while(fin.hasNext())
+//		{
+//			System.out.println(fin.nextLine());
+//		}
+		 
+		PartyQueue Party = new PartyQueue();
+		
+		//nextCommand=fin.next();
+		while(!(nextCommand.equalsIgnoreCase("q")))
+		{
+			nextCommand=fin.next();
+			System.out.println(nextCommand);
+			
+			if(nextCommand.equalsIgnoreCase("a")) 
+			{
+				Party.enqueue(fin);
+				toa=fin.nextInt();
+				partySize=fin.nextInt();
+				partyName=fin.next();
+			}
+			if(nextCommand.equalsIgnoreCase("t"))
+			{
+				Party.dequeue();
+				seatTime=fin.nextInt();
+			}
+		}
+		//System.out.println(Party.getFront());
 	}
-
 }
