c160f972ad625522fcd3de2a7d8389dfa200653e
Dylan Frechette <1999djf@gmail.com>
2019-04-10 17:20:34 -0400

    Made help function and set up statements to check for inputs

--- CampPosanivee/src/CampMain.java
+++ CampPosanivee/src/CampMain.java
@@ -1,14 +1,46 @@
 import java.io.*;
 import java.util.*;
 
+
+
 public class CampMain {
 
-	public static void main(String[] args) throws FileNotFoundException
-	{	
-		Scanner fin=new Scanner (new FileReader("camp.txt"));
+	//Displays Help Screen with prompts to input
+	public static Scanner Help() throws FileNotFoundException
+{
+	Scanner fin=new Scanner (new FileReader("camp.txt"));
 	while (fin.hasNextLine()){
 		System.out.println(fin.nextLine());
 	}
+	return fin;
+}
+	public static void main(String[] args) throws FileNotFoundException
+	{	
+	Help();
+	System.out.println("Please enter what you would like to do: ");
+	Scanner cin=new Scanner(System.in);
+while(cin != 'Q'){
+	if (cin == 'H') Help();
+
+	else if (cin == 'E')
+
+	else if (cin == 'W')
+
+	else if (cin == 'D')
+
+	else if (cin == 'A')
+
+	else if (cin == 'L')
+
+	else if (cin == 'S')
+
+	else if (cin == 'P')
+
+	else System.out.println("That isn't a valid input, please try again: ");
+
+	Help();
+	
+}
 	}
 
 }
