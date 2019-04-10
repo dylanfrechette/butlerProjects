497cc6faea2ec7eff35e5a94cd21799895fd6647
Dylan Frechette <1999djf@gmail.com>
2019-04-10 17:26:35 -0400

    formatted to make cleaner

--- CampPosanivee/src/CampMain.java
+++ CampPosanivee/src/CampMain.java
@@ -19,28 +19,33 @@ public class CampMain {
 	Help();
 	System.out.println("Please enter what you would like to do: ");
 	Scanner cin=new Scanner(System.in);
-while(cin != 'Q'){
-	if (cin == 'H') Help();
+		while(cin != 'Q')
+		{
+			if (cin == 'H') Help();
 
-	else if (cin == 'E')
+			else if (cin == 'E')
 
-	else if (cin == 'W')
+			else if (cin == 'W')
 
-	else if (cin == 'D')
+			else if (cin == 'D')
 
-	else if (cin == 'A')
+			else if (cin == 'A')
 
-	else if (cin == 'L')
+			else if (cin == 'L')
 
-	else if (cin == 'S')
+			else if (cin == 'S')
 
-	else if (cin == 'P')
+			else if (cin == 'P')
 
-	else System.out.println("That isn't a valid input, please try again: ");
+			else if (cin != 'Q')
+			{
+				System.out.println("That isn't a valid input, please try again: ");
+				cin(System.in);
+			}
 
-	Help();
-	
-}
+			Help();
+			cin(System.in);
+			}
 	}
 
 }
