bb515938e7bac14c8bc6683ac263e25bbf913772
Dylan Frechette <1999djf@gmail.com>
2019-04-10 17:49:29 -0400

    formatted to make cleaner, added new function

--- CampPosanivee/src/CampMain.java
+++ CampPosanivee/src/CampMain.java
@@ -14,6 +14,15 @@ public class CampMain {
 	}
 	return fin;
 }
+	public static int avgAge()
+	{
+		ing avgAge = 0;
+		for (int i=0; i < numCampers; i++)
+		{
+		avgAge = avgAge + camperAge[i];
+		}
+		return avgAge;
+	}
 	public static void main(String[] args) throws FileNotFoundException
 	{	
 	Help();
@@ -24,19 +33,23 @@ public class CampMain {
 			if (cin == 'H') Help();
 
 			else if (cin == 'E')
+				System.out.println("Enroll a new camper: ");
+
 
 			else if (cin == 'W')
+				System.out.println("Withdraw a camper: ");
 
 			else if (cin == 'D')
+				System.out.println("Age and Gender of selected camper: ");
 
 			else if (cin == 'A')
-
+				System.out.println("Average age of campers: "+ avgAge());
 			else if (cin == 'L')
-
+				System.out.println("Alphabetical list of campers: ");
 			else if (cin == 'S')
-
+				System.out.println("Number of boys: "+boys+" Number of girls: "+girls);
 			else if (cin == 'P')
-
+				System.out.println("List of campers in preorder: ")
 			else if (cin != 'Q')
 			{
 				System.out.println("That isn't a valid input, please try again: ");
@@ -45,7 +58,7 @@ public class CampMain {
 
 			Help();
 			cin(System.in);
-			}
+		}
+	System.out.println("Finished editing Camp Roster");	
 	}
-
 }
