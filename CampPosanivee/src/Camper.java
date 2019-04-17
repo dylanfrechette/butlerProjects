986370841731ee4873b70c9555d7f55485f70779
Dylan Bakr <dylan_bakr@comcast.net>
2019-04-17 13:49:34 -0400

    Update Camper.java

--- CampPosanivee/src/Camper.java
+++ CampPosanivee/src/Camper.java
@@ -1,31 +1,58 @@
 import java.io.*;
 import java.util.*;
 
-public class Camper  {
-    String name;
+public class Camper implements Comparable
+{
     int age;
-    String sex;
-
-public static Scanner CampBoiz() throws FileNotFoundException{
-    Scanner CampyBoys = new Scanner(new FileReader("Campers.txt"));
-return CampyBoys;
-}
-
-    public Comparable [] Campers(String callsign, int oldness, String gender){
-        Comparable [] camper= new Comparable [] {callsign, oldness, gender};
-        name = callsign;
-        age = oldness;
-        sex = gender;
-        return camper;
+    char gender;
+    String name;
+    //default constructor
+    public Camper()
+    {
+        age = 0;
+        gender = 'A';
+        name = "";
+    }
+    //default constructor with parameters
+    public Camper(int a, char g, String n)
+    {
+        age = a;
+        gender = g;
+        name = n;
+    }
+    //default constructor for a scanner
+    public Camper(Scanner s)
+    {
+        name = s.next();
+        age = s.nextInt();
+        gender = s.next().charAt(0);
     }
 
-    public String getName(){
-        return name;
+    public int compareTo(Object x)
+    {
+        if (x instanceof Camper)
+        {
+            Camper xCamper = (Camper) x;
+            return this.name.compareTo(xCamper.name);
+        }
+        else
+        {
+            System.out.println("Error not a camper!");
+        }
+        return 0;
     }
-    public int getAge(){
-        return age;
-    }
-    public String getSex(){
-        return sex;
+
+    public int getAge()
+    { return age; }
+
+    public char getSex()
+    { return gender; }
+
+    public String getName()
+    { return name; }
+
+    public String toString()
+    {
+        return ("Name: "+name+" | Age: "+age+" | Gender: "+gender);
     }
 }
