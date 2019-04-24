3262d49fac4b74c321ba6476ed0b2f4ec303b192
Dylan Bakr <dylan_bakr@comcast.net>
2019-04-24 14:03:09 -0400

    jerryMain

---
+++ Jerry/jerryMain.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class jerryMain {
+
+    public static void main(String[] args){
+
+        System.out.println("Welcome to Jerry-Tac-Toe");
+        //draw game board
+        //window pops up with two buttons asking if user wants to go first or second
+        //user interacts with game window
+        //game window closes upon end of game and thanks for playing
+        //new window to play again with yes/no buttons
+    }
+}
