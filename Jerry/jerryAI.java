dcace2fac044fdecdc04b507f27c6de41af37536
Dylan Frechette <1999djf@gmail.com>
2019-04-29 09:00:55 -0400

    AI maybe doner?

--- Jerry/jerryAI.java
+++ Jerry/jerryAI.java
@@ -21,7 +21,7 @@ public static void AI()
     int moveCount=0;
     boolean playing=true;
     boolean emptySpot=true;
-    int [] winCheck= new int []{{1,2,3},{1,4.8},{1,5,9},{2,4,7},{2,6,9},{3,5,7},{3,6,9},{4,5,6},{7,8,9}};
+    int [] winCheck= new int []{{1,2,3},{1,4,8},{1,5,9},{2,4,7},{2,6,9},{3,5,7},{3,6,9},{4,5,6},{7,8,9}};
     //Makes sets to check for subsets as wincons, compares them, true is a win, false is a continue
     Set<Integer> playerSet = new TreeSet<Integer>();
     Set<Integer> AISet = new TreeSet<Integer>();
@@ -32,17 +32,15 @@ public static void AI()
         {
         if((isTurnFirst()==true) || (isTurnSecond() == true))
             {
-            if( moveCount==0) selectedSpot=5;
-            else
-                {
-                selectedSpot=1+(spots.length*rand());
-                while(emptySpot==false) selectedSpot=1+(spots.length*rand());
-                }
+             selectedSpot=1+(spots.length*rand());
+             while(emptySpot==false) selectedSpot=1+(spots.length*rand());
             }
         selectedSpots.add(selectedSpot);
         }
         winCons(playerSet,winSet);
+        if(winCons(playerSet,winSet)==true) System.out.println("Congratulations, you win!");
         wincons(AISet,winSet);
+        if(winCons(AISet,winSet)==true) System.out.println("You lose :(");
         moveCount++;
     }
 }
@@ -70,12 +68,12 @@ private boolean playing()
 }
 private static boolean isTurnFirst()
 {
-    if(selectedSpots.length()>=playerMoves.length()) return true;
+    if(selectedSpots.size()==playerMoves.size()) return true;
     else return false;
 }
 private static boolean isTurnSecond()
 {
-    if(selectedSpots.length()<playerMoves.length()) return true;
+    if(selectedSpots.size()<playerMoves.size()) return true;
     else return false;
 }
 }
